81b8a3324a053b1a530fce9fe58b9ae9f02d4d21
Swaroop P <pswaroop1994@gmail.com>
2022-03-03 13:11:59 +0530

    Create java-driver-code.java

---
+++ java-driver-code.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  	// The main function reads the input in the required format.
+  	// Just complete the solve function below.
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter wr = new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        for(int t_i = 0; t_i < T; t_i++)
+        {
+            String str = br.readLine();
+            char out_ = solve(str);
+            System.out.println(out_);
+         }
+         wr.close();
+         br.close();
+    }
+  
+    static char solve(String s){
+      	// Your code goes here
+        char result = ' ';
+        return result;
+    }
+}
