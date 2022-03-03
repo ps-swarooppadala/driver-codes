444e4e66b165390a20b22b84593b3bff1b7c3770
Swaroop P <pswaroop1994@gmail.com>
2022-03-03 15:57:52 +0530

    Create js-starter-code.js

---
+++ js-starter-code.js
@@ -0,0 +1,16 @@
+// JavaScript Driver Code
+
+function solve(s){
+    // Your code goes here
+    // s is the given input string
+    return " "
+}
+
+// The following snippet reads the input in the required format. 
+// Just complete the solve function above. 
+const input = parseInt(prompt());
+for (let i = 0; i < input; i++) {
+    let s = prompt();
+    let answer = solve(s);
+    console.log(answer)
+}
