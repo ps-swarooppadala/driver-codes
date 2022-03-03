17e2f071be1f5c18f4e18ba2f14f340a9821aee0
Swaroop P <pswaroop1994@gmail.com>
2022-03-03 13:34:12 +0530

    Create cpp-driver-code.cpp

---
+++ cpp-driver-code.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include<bits/stdc++.h>
+using namespace std;
+
+char solve (string s) {
+   // Your code goes here
+   return ' ';
+}
+
+// The main function reads the input in the required format.
+// Just complete the solve function above.
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    for(int t_i = 0; t_i < T; t_i++)
+    {
+        string s;
+        cin >> s;
+        char out_;
+        out_ = solve(s);
+        cout << out_;
+        cout << "\n";
+    }
+}
