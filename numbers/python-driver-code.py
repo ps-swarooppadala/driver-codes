125d6e853667c355c02006b8e47f396d4e8c5010
chandu294 <103244175+chandu294@users.noreply.github.com>
2022-04-08 12:54:12 +0530

    Update python-driver-code.py

--- numbers/python-driver-code.py
+++ numbers/python-driver-code.py
@@ -1,15 +1,13 @@
-# Python Driver Code
-
-def solve(n: int) -> str:
-  # Your code goes here
-  # n is the given input
-  return "Special"
-
-# The following snippet reads the input in the required format. 
-# Just complete the solve function above. 
-
-T = int(input())
-for i in range(T):
-  test_case = input()
-  answer = solve(test_case)
-  print(answer)
+def isMagic (n):
+sum=0;
+while (n>0 or sum>9):
+if(n==0):
+n=sum;
+sum=0;
+sum=sum+n%10;
+n=int(n/10);
+written True if(sum=1) else False
+n=1234;
+if(isMagic(n)):
+print("MagicNumber");
+else:print("not a MagicNumber");
